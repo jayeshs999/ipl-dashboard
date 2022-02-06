d08f718981f6ee468c3b46b4f5c0eecc01aad5bf
Jayesh Singla <jayesh11.3.01@gmail.com>
2022-02-07 01:38:09 +0530

    Finish README and update dependencies

--- frontend/src/app/player/player-info/batting-stats.config.ts
+++ frontend/src/app/player/player-info/batting-stats.config.ts
@@ -2,9 +2,8 @@ export const COLORS = [
     'rgb(255, 99, 132)',
     'rgb(255, 159, 64)',
     'rgb(255, 205, 86)',
-    'rgb(75, 192, 192)',
 ]
-export const LEGEND_NAMES = ['< 20', '<=30', '<=50', '>50']
+export const LEGEND_NAMES = ['< 30', '>=30 and <=50', '>50']
 
 export const BattingsStatsConfig = (match_ids: any, runs: any):any => {
     return {
@@ -14,14 +13,12 @@ export const BattingsStatsConfig = (match_ids: any, runs: any):any => {
             datasets: [{
                 data: runs,
                 backgroundColor: runs.map((ele:any)=>{
-                    if(ele < 20)
+                    if(ele < 30)
                         return COLORS[0];
-                    else if(ele < 31)
+                    else if(ele <= 50)
                         return COLORS[1];
-                    else if(ele < 51)
-                        return COLORS[2];
                     else
-                        return COLORS[3];
+                        return COLORS[2];
                 }),
             }]
         },
