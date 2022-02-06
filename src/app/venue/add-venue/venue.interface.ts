4deba2798810f183071277d7706c5c08c79b3277
Jayesh Singla <jayesh11.3.01@gmail.com>
2022-02-06 18:30:19 +0530

    Restructure

--- src/app/venue/add-venue/venue.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Venue{
-    name? : string;
-    country? : string;
-    city?: string;
-    capacity? : number;
-}
--- src/app/venue/venue-list/venue-list.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface VenueList{
-    venue_id : number;
-    venue_name : string;
-    address : string;
-    capacity : number;
-    total_matches : number;
-}
