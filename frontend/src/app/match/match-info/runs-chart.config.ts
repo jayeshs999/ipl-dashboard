92ecca6cc8b5bbb40c3148f84d9b84054cbff212
Jayesh Singla <jayesh11.3.01@gmail.com>
2022-02-07 17:01:37 +0530

    Minor fix in extras

--- frontend/src/app/match/match-info/runs-chart.config.ts
+++ frontend/src/app/match/match-info/runs-chart.config.ts
@@ -6,7 +6,7 @@ export const RunsChartOptions = (runs:any, team:any) : any =>{
             labels: ["Ones", "Twos", "Threes", "Fours", "Sixes", "Extras"],
             datasets: [{
                 label: 'Innings 1',
-                data: [parseInt(runs.ones) || 0, parseInt(runs.twos) || 0, parseInt(runs.threes) || 0, parseInt(runs.fours) || 1, parseInt(runs.sixes) || 0, parseInt(runs.extras) || 0],
+                data: [parseInt(runs.ones) || 0, parseInt(runs.twos) || 0, parseInt(runs.threes) || 0, parseInt(runs.fours) || 1, parseInt(runs.sixes) || 0, parseInt(runs.extra_runs) || 0],
                 backgroundColor: ["red","blue", "orange", "pink", "yellow", "black"],
                 borderWidth: 1
             }]
